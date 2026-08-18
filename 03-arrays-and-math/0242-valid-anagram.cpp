7cd2dc6e51f1fda341bde9ba763ee98feb469b36
Mertcan İLHAN <mertcanilhan14@gmail.com>
2026-08-18 11:40:37 +0300

    adding 242 valid anagram solution

---
+++ 03-arrays-and-math/0242-valid-anagram.cpp
@@ -0,0 +1,41 @@
+/*
+ * GitHub: https://github.com/m3ill
+ * Pattern: Hash Map / Frequency Array
+ * Time Complexity: O(n) (n: string uzunluğu)
+ * Space Complexity: O(k) (k: benzersiz karakter sayısı, en fazla O(1) [26 karakter])
+ */
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool is_zero(std::unordered_map<char,int> &freq){
+        for(const auto &[key,value] : freq){
+            if(value != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isAnagram(string s, string t) {
+        size_t s_size = s.size();
+        size_t t_size = t.size();
+        if(s_size != t_size) return false;
+
+        std::unordered_map<char, int> freq;
+
+        for(char &c : s){
+            freq[c]++;
+        }
+
+        for(char &c: t){
+            freq[c]--;
+        }
+
+        return is_zero(freq) ? true : false;
+
+    }
+};
