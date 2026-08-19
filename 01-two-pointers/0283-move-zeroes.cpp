10d6eacf498e7ebcc4fb31994bd098e3db37b21a
Mertcan İLHAN <mertcanilhan14@gmail.com>
2026-08-19 11:02:10 +0300

    adding 283 move zeroes solution

---
+++ 01-two-pointers/0283-move-zeroes.cpp
@@ -0,0 +1,31 @@
+/*
+* Problem: LeetCode 283 - Move Zeroes
+ * Author: Mertcan
+ * GitHub: https://github.com/m3ill
+ * Pattern: Two Pointers
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+#include <vector>
+using std::vector;
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        //bu yerleştirilecek yerleri tutacak
+        int left = 0;
+        //bu sayı mı 0 mı ona bakacak
+        int right = 0;
+
+        while(right < nums.size()){
+            if(nums[right] != 0){
+                std::swap(nums[right], nums[left]);
+
+                left++;
+            }
+            right++;
+        }
+    }
+};
